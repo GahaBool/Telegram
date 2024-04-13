04d4e0796bf4eaa2744435d0a982f93e2a73b04f
Egor Tirskih_laptop <egortirskih1984@gmail.com>
2024-04-13 16:21:21 +0800

    add all file with laptoop

--- keybs/reply.py
+++ keybs/reply.py
@@ -1,12 +1,16 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 start_kb = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text="Информация о аккаунте"),
-            KeyboardButton(text="Меню"),
-            KeyboardButton(text="Донат: "),
         ],
+        {
+            KeyboardButton(text="Меню: "),
+            KeyboardButton(text="Донат: "),
+        }
     ],
+    resize_keyboard=True,
+    input_field_placeholder="Что вас интересует?"
 )
